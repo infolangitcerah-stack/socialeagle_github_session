bd86189ec7c2287db77510be31f9a6436aec33f7
infolangitcerah-stack <info.langitcerah@gmail.com>
2025-09-08 11:14:34 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+
+
+name = "Dom"
+print('name')
+print(name)
+
+age = 58
+print('age')
+print(age)
+
+race = "Malaysian"
+
+print('My name is', name, "and I am", age, "years old")
+print('My name is', name, "and I am", age, "years old" and "I am", race)
+print('My name is', name, "and I am", age, "years old" and "I am", race)
+print('Hello, world!')
+print("This is a test for Thiru")
+print('My name is', name, ", I am", age, "years old", "and I am a", race)
+print("My name is", name, "I am", age, "years old", "and I am a", race)
